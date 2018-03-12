f13af1be61bfe1ac8dae1b4b254ab4e066eecf40
Rafael Chies <rafaelchies@rchies-OSX.local>
2018-03-12 15:52:07 -0300

    as

--- src/main/java/br/redhat/consulting/Lab02Rest.java
+++ src/main/java/br/redhat/consulting/Lab02Rest.java
@@ -1,11 +1,15 @@
 package br.redhat.consulting;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Lab02Rest extends RouteBuilder {
+
+	private static final String STOCK_JSON = "{\"symbol\":\"rht\", \"name\":\"redhat\", \"price\":\"158\"}";
 
 	@Override
 	public void configure() throws Exception {
@@ -16,10 +20,20 @@ public class Lab02Rest extends RouteBuilder {
 			.bindingMode(RestBindingMode.json)
 			.dataFormatProperty("printPretty", "true");
 		
-		rest()
+		rest("/rest")
 			.get("/say").route()
 				.setBody(simple("Hello World"))
-		.end();
+			.endRest()
+			.get("/say/{message}").route()
+				.setBody(simple("Hello ${header.message}"))
+			.endRest()
+			.get("/stock").produces(MediaType.APPLICATION_JSON_VALUE).route()
+				.setBody(constant(STOCK_JSON))
+				.unmarshal().json(JsonLibrary.Jackson)
+			.endRest();
+		
+		
+			
 	}
 
 }
