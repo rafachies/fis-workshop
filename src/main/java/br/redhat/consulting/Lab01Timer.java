3de2bc99a0ba48b8346681348557750763bb188d
Rafael Chies <rafaelchies@rchies-OSX.local>
2018-03-12 14:43:26 -0300

    as

--- src/main/java/br/redhat/consulting/Lab01Timer.java
+++ src/main/java/br/redhat/consulting/Lab01Timer.java
@@ -3,15 +3,16 @@ package br.redhat.consulting;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import br.redhat.consulting.util.BodyProcessor;
+
 @Component
 public class Lab01Timer extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-	
-		from("timer:workshop?period=5000")
-			.log("Hello Camel!");
+		from("timer:blue?period=5000").routeId("timerRoute")
+			.process(new BodyProcessor())
+			.log("${body}");
 		
 	}
-
 }
