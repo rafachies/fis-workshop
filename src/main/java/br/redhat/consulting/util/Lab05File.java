bfec854a94b78e025d65885b3116bd52dd954e71
Rafael Chies <rafaelchies@rchies-OSX.local>
2018-03-13 15:32:56 -0300

    as

---
+++ src/main/java/br/redhat/consulting/util/Lab05File.java
@@ -0,0 +1,21 @@
+package br.redhat.consulting.util;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Lab05File extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("file:files/live?fileName=stock.xml")
+			.choice()
+			.when(xpath("//price > 100"))
+				.to("file:files/live/expensive/")
+			.otherwise()
+				.to("file:files/live/cheap/")
+		.endChoice();
+
+	}
+
+}
