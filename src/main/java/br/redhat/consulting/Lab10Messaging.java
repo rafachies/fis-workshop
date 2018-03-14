b9397c04a149361a004584fbd098c17159abc2f1
Rafael Chies <rafaelchies@rchies-OSX.local>
2018-03-14 15:22:02 -0300

    hystrix

--- src/main/java/br/redhat/consulting/Lab10Messaging.java
+++ src/main/java/br/redhat/consulting/Lab10Messaging.java
@@ -10,12 +10,26 @@ public class Lab10Messaging extends RouteBuilder {
 	public void configure() throws Exception {
 		
 		rest("/amq")
-			.get("/queue/{message}").to("direct:sendQueue");
+			.get("/queue/{message}").to("direct:sendQueue")
+			.get("/topic/{message}").to("direct:sendTopic");
 		
 		from("direct:sendQueue")
 			.setBody(simple("${header.message}"))
 			.to("activemq:queue:redhat?deliveryPersistent=false&exchangePattern=InOnly")
 			.log("Body returned: ${body}");
+		
+		from("direct:sendTopic")
+			.setBody(simple("${header.message}"))
+			.to("activemq:topic:jboss?exchangePattern=InOnly");
+		
+		//Two clientes expecting a topic message
+		from("activemq:topic:jboss")
+			.routeId("lopes")
+			.log("Message received: ${body}");
+		
+		from("activemq:topic:jboss?clientId=chies&durableSubscriptionName=chies")
+			.routeId("chies")
+			.log("Message received: ${body}");
 		
 		
 		from("activemq:queue:redhat")
